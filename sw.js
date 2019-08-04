ee7afa153616e81fccd3182cd9ed8ce0d95bb7ee
kinoras <39506429+kinoras@users.noreply.github.com>
2019-08-04 23:22:19 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -11,16 +11,16 @@ self.addEventListener('install', function(e) {
        '/js/script.js',
        '/js/jquery.js',
        '/js/vue.js',
-       '/img/apple-icon-57x57.png',
-       '/img/apple-icon-60x60.png',
-       '/img/apple-icon-72x72.png',
-       '/img/apple-icon-76x76.png',
-       '/img/apple-icon-114x114.png',
-       '/img/apple-icon-120x120.png',
-       '/img/apple-icon-144x144.png',
-       '/img/apple-icon-152x152.png',
-       '/img/apple-icon-180x180.png',
-       '/img/android-icon-192x192.png'
+       '/imgs/apple-icon-57x57.png',
+       '/imgs/apple-icon-60x60.png',
+       '/imgs/apple-icon-72x72.png',
+       '/imgs/apple-icon-76x76.png',
+       '/imgs/apple-icon-114x114.png',
+       '/imgs/apple-icon-120x120.png',
+       '/imgs/apple-icon-144x144.png',
+       '/imgs/apple-icon-152x152.png',
+       '/imgs/apple-icon-180x180.png',
+       '/imgs/android-icon-192x192.png'
      ]);
    })
  );
