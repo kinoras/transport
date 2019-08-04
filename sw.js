2a3dbd00f9866d443a701ba3bdf060ff49407d77
kinoras <39506429+kinoras@users.noreply.github.com>
2019-08-04 22:59:40 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,27 @@
-//sw
+importScripts('/cache-polyfill.js');
+
+
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('airhorner').then(function(cache) {
+     return cache.addAll([
+       '/',
+       '/index.html',
+       '/style.css',
+       '/js/script.js',
+       '/js/jquery.js',
+       '/js/vue.js',
+       '/img/apple-icon-57x57.png',
+       '/img/apple-icon-60x60.png',
+       '/img/apple-icon-72x72.png',
+       '/img/apple-icon-76x76.png',
+       '/img/apple-icon-114x114.png',
+       '/img/apple-icon-120x120.png',
+       '/img/apple-icon-144x144.png',
+       '/img/apple-icon-152x152.png',
+       '/img/apple-icon-180x180.png',
+       '/img/android-icon-192x192.png',
+     ]);
+   })
+ );
+});
